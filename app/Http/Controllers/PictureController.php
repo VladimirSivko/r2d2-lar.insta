0db8ce07e35b4b7a0a16605ebee8ef1820f46029
Vasyl <admin@gmail.com>
2018-12-26 17:54:36 +0200

    change PictureController

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Picture;
 
 class PictureController extends Controller {
 
@@ -24,6 +25,23 @@ class PictureController extends Controller {
      */
     public function index(Request $request) {
 	return view('pictures.index');
+    }
+
+    public function store(Request $request) {
+
+	$picture = new Picture($request->except('img'));
+	if ($request->hasFile('img')) {
+	    $file = $request->file('img');
+	    $destinationPath = public_path() . '/house/uploads/';
+	    $filename = str_random(20) . '.' . $file->getClientOriginalExtension() ?: 'png';
+	    $page->img = $filename;
+	    if ($request->hasFile('img')) {
+		$request->file('img')->move($destinationPath, $filename);
+	    }
+	}
+	$picture->save();
+
+	return redirect()->route('picture.index');
     }
 
 }
