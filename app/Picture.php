84e24b393b5b03d3f8e0c47dd5661aa329a65fb9
Vasyl <admin@gmail.com>
2018-12-26 17:19:47 +0200

    relations Picture.php and User.php

--- app/Picture.php
+++ app/Picture.php
@@ -4,12 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Picture extends Model
-{
+class Picture extends Model {
+
     /**
-   * Массово присваиваемые атрибуты.
-   *
-   * @var array
-   */
-  protected $fillable = ['path_to_picture'];
+     * Массово присваиваемые атрибуты.
+     *
+     * @var array
+     */
+    protected $fillable = ['path_to_picture'];
+
+    /**
+     * Получить пользователя - владельца данной задачи
+     */
+    public function user() {
+	return $this->belongsTo(User::class);
+    }
+
 }
